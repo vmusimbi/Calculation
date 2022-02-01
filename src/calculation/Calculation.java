9332567d5bd73c4a4a806789cb303dad82088a35
vmusimbi <52039126+vmusimbi@users.noreply.github.com>
2022-02-01 08:48:01 +0300

    Initial commit

---
+++ src/calculation/Calculation.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+/**
+ *
+ * @author VALENTINE
+ */
+public class Calculation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        int y = 5;
+        int sum = x + y;
+        int subtraction = x - y;
+        int multiplication = x * y;
+        int division = x / y;
+        int modulo = x % y;
+        
+        System.out.println("Sum : " + sum );
+         System.out.println("Subtraction : " + subtraction);
+          System.out.println("Multiplication : " + multiplication);
+           System.out.println("Division : " + division);
+            System.out.println("Modulo : " + modulo);
+        
+        
+        
+    }
+    
+}
